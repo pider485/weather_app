6949f2e07c77c4d286b836faa1f8dbc6b3aa5869
pider485 <oleksijzolotarov4@gmail.com>
2024-05-18 12:31:52 +0300

    add search

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from kivymd.uix.backdrop.backdrop import MDCard
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 
+class WeatherCard(MDCard):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 class MainScreen(MDScreen):
     def __init__(self, *args, **kwargs):
@@ -16,6 +20,5 @@ class WeatherApp(MDApp):
         Builder.load_file('style.kv')
         self.screen = MainScreen("main_screen")
         return self.screen
-
 
 WeatherApp().run()
