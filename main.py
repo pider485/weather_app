56601c11fe552cf79410e00ecc7422dc199b18e3
pider485 <oleksijzolotarov4@gmail.com>
2024-05-25 12:31:55 +0300

    sezrch weather

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
+from config import API_KEY, API_URL
+import requests
 
 class WeatherCard(MDCard):
     def __init__(self, *args, **kwargs):
@@ -12,7 +14,17 @@ class MainScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-
+    def weather_search(self):
+        city = self.ids.city_field.text.strip().lower()
+        api_params={
+            "q": city,
+            "appid": API_KEY
+        }
+        data = requests.get(API_URL,api_params)
+        respon = data.json()
+        print(respon)
+        description = respon["weather"][0]["description"]
+        self.ids.Weather_card.ids.label.text = description
 
 class WeatherApp(MDApp):
     def build(self):
