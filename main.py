32620f0b12672852e48a63069e0d6788364b9901
pider485 <oleksijzolotarov4@gmail.com>
2024-06-01 12:33:50 +0300

    показ погоди

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ from config import API_KEY, API_URL
 import requests
 
 class WeatherCard(MDCard):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, description, icon, temp, rain, wind, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.ids.desc_text.text = description
+        self.ids.temp_text.text = f"{temp}°C"
+        self.ids.rain_text.text = f"Ймовірність опадів: {rain*100}%"
+        self.ids.wind_text.text = f"Швидкість вітру:{wind}м/с"
+        self.ids.weather_icon.source = f"https://openweathermap.org/img/wn/{icon}@2x.png"
+
 
 class MainScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def weather_search(self):
+        self.ids.Weather_carusel.clear_widgets()
         city = self.ids.city_field.text.strip().lower()
         api_params={
             "q": city,
@@ -24,7 +31,16 @@ class MainScreen(MDScreen):
         respon = data.json()
         print(respon)
         description = respon["weather"][0]["description"]
-        self.ids.Weather_card.ids.label.text = description
+        icon = respon["weather"][0]["icon"]
+        temp = respon["main"]["temp"]
+        if 'rain' in respon:
+            rain = respon["rain"]["1h"]
+        else:
+            rain= 0
+        wind = respon["wind"]["speed"]
+
+        new_card = WeatherCard (description,icon,temp,rain,wind)
+        self.ids.Weather_carusel.add_widget(new_card)
 
 class WeatherApp(MDApp):
     def build(self):
